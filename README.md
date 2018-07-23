3361ffac5ce19580eab439b86fa351d9e912e346
Enayetullah Rasuli <Enayetullah.Rasuli@softwareag.com>
2018-07-23 10:13:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Commute Service - Commute Service Discovery
+
+This service offers service discovery and load balancing for the microservices
+
+## Build
+`mvn package`
+
+## Run
+`mvn spring-boot:run`
---
+++ src/main/java/com/commute/servicediscovery/ServiceDiscoveryApp.java
@@ -0,0 +1,35 @@
+package com.commute.servicediscovery;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+@EnableEurekaServer
+public class ServiceDiscoveryApp {
+
+    /**
+     * default entry point
+     *
+     * @param args unused
+     * @throws UnknownHostException
+     */
+    @SuppressWarnings("squid:S1313") // IP addresses should not be hardcoded => In this case it is ok
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(ServiceDiscoveryApp.class);
+        Environment env = app.run(args).getEnvironment();
+
+        log.info(
+                "\n----------------------------------------------------------\n\t" + "Application '{}' is running! Access URLs: \n\t"
+                        + "Local: \t\thttp://127.0.0.1:{} \n\t" + "External: \thttp://{}:{}"
+                        + "\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"), env.getProperty("server.port"), InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
